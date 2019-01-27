3b2276c2686c478c0c4362132f776cf8d6b9d422
OndrejNepozitek <ondra@nepozitek.cz>
2019-01-27 17:50:27 +0100

    Removed unused code

--- GraphPlanarityTesting/PlanarityTesting/BoyerMyrvold/BucketSort.cs
+++ GraphPlanarityTesting/PlanarityTesting/BoyerMyrvold/BucketSort.cs
@@ -5,33 +5,6 @@
 
 	public class BucketSort
 	{
-		public static T[] Sort<T>(Dictionary<T, int> elements, int bucketsCount)
-		{
-			var buckets = new List<T>[bucketsCount];
-
-			for (int i = 0; i < bucketsCount; i++)
-			{
-				buckets[i] = new List<T>();
-			}
-
-			foreach (var element in elements)
-			{
-				buckets[element.Value].Add(element.Key);
-			}
-
-			var result = new List<T>();
-
-			foreach (var bucket in buckets)
-			{
-				foreach (var element in bucket)
-				{
-					result.Add(element);
-				}
-			}
-
-			return result.ToArray();
-		}
-
 		public static T[] Sort<T>(IEnumerable<T> elements, Func<T, int> valueSelector, int bucketsCount)
 		{
 			var buckets = new List<T>[bucketsCount];
